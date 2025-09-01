42553bd09f3397a0de14a67490081fcd7cad752b
apm1432 <akshaymerat50@gmail.com>
2025-09-01 13:46:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1552,7 +1552,7 @@ async def universal_drm_handler(bot: Client, m: Message):
          
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}"
+                url = f"https://drmjion-botupdatevip.vercel.app/api?url={url}&token={cptoken}"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
