d3e738dc6ea04d50c6216da0dce897f72ae547a2
apm1432 <akshaymerat50@gmail.com>
2025-09-16 19:53:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -477,29 +477,29 @@ async def start(bot, m: Message):
        f"Use Command : /help to get started 🌟\n\n"
        f"If you face any problem contact -  [PATIL](https://t.me/Driven_soulbot)\n", disable_web_page_preview=True, reply_markup=BUTTONSCONTACT
     )
-    else:
-        await asyncio.sleep(2)
-        await start_message.edit_text(
-           f" 🎉 Welcome {m.from_user.first_name} to DRM Bot! 🎉\n\n"
-           f"You can have access to download all Non-DRM+AES Encrypted URLs 🔐 including\n\n"
-           f"Use Command : /help to get started 🌟\n\n"
-           f"<blockquote>• 📚 Appx Zip+Encrypted Url\n"
-           f"• 🎓 Classplus DRM+ NDRM\n"
-           f"• 🧑‍🏫 PhysicsWallah DRM\n"
-           f"• 📚 CareerWill + PDF\n"
-           f"• 🎓 Khan GS\n"
-           f"• 🎓 Study Iq DRM\n"
-           f"• 🚀 APPX + APPX Enc PDF\n"
-           f"• 🎓 Vimeo Protection\n"
-           f"• 🎓 Brightcove Protection\n"
-           f"• 🎓 Visionias Protection\n"
-           f"• 🎓 Zoom Video\n"
-           f"• 🎓 Utkarsh Protection(Video + PDF)\n"
-           f"• 🎓 All Non DRM+AES Encrypted URLs\n"
-           f"• 🎓 MPD URLs if the key is known (e.g., Mpd_url?key=key XX:XX)</blockquote>\n\n"
-           f"🚀 You are not subscribed to any plan yet!\n\n"
-           f"<blockquote>💵 Monthly Plan: 800</blockquote>\n\n"
-           f"If you want to buy membership of the bot, feel free to contact the Bot Admin.\n", disable_web_page_preview=True, reply_markup=keyboard
+    
+    await asyncio.sleep(2)
+    await start_message.edit_text(
+        f" 🎉 Welcome {m.from_user.first_name} to DRM Bot! 🎉\n\n"
+        f"You can have access to download all Non-DRM+AES Encrypted URLs 🔐 including\n\n"
+        f"Use Command : /help to get started 🌟\n\n"
+        f"<blockquote>• 📚 Appx Zip+Encrypted Url\n"
+        f"• 🎓 Classplus DRM+ NDRM\n"
+        f"• 🧑‍🏫 PhysicsWallah DRM\n"
+        f"• 📚 CareerWill + PDF\n"
+        f"• 🎓 Khan GS\n"
+        f"• 🎓 Study Iq DRM\n"
+        f"• 🚀 APPX + APPX Enc PDF\n"
+        f"• 🎓 Vimeo Protection\n"
+        f"• 🎓 Brightcove Protection\n"
+        f"• 🎓 Visionias Protection\n"
+        f"• 🎓 Zoom Video\n"
+        f"• 🎓 Utkarsh Protection(Video + PDF)\n"
+        f"• 🎓 All Non DRM+AES Encrypted URLs\n"
+        f"• 🎓 MPD URLs if the key is known (e.g., Mpd_url?key=key XX:XX)</blockquote>\n\n"
+        f"🚀 You are not subscribed to any plan yet!\n\n"
+        f"<blockquote>💵 Monthly Plan: 800</blockquote>\n\n"
+        f"If you want to buy membership of the bot, feel free to contact the Bot Admin.\n", disable_web_page_preview=True, reply_markup=keyboard
     )
 
 @bot.on_message(filters.command(["upgrade"]))
